9510ff7a405db052683a20b361c2bd42414c9bfa
Black <53127242+Aubert-co@users.noreply.github.com>
2021-11-30 22:52:53 -0300

    Update addASchedule.js

--- App/controller/addASchedule.js
+++ App/controller/addASchedule.js
@@ -1,15 +1,20 @@
-module.exports = function ApiAddSchedule(req,res){
+module.exports = async function ApiAddSchedule(req,res){
     try{
         const {date,content} = req.body
-        console.log(date)
-        if(date === '' )return res.status(400).send({msg:'invalid date'})
-        
+        const [year,month,day] = date.split('/')
+        const VerifyDate = (value,length)=>{
+            if(Number(value) && value.length === length )return true
+
+            return false
+        }
+        if(date === '' || !VerifyDate(day,2) || !VerifyDate(year,4) || !VerifyDate(month,2)  )return res.status(400).send({msg:'invalid date'})
+       
+    
         if(content === '')return res.status(400).send({msg:'invalid content'})
 
-        res.status(200).send({msg:'sucess'})
+        res.status(200).send({msg:'sucess',data:[]})
     }catch(err){
         res.status(500).send({msg:'something went wrong'})
     }
-
-
 }
+
