4241f1d436f4e384c0b247430fede1b6e1a7f7d8
Black <53127242+Aubert-co@users.noreply.github.com>
2021-11-30 22:52:26 -0300

    Update rotaAddSchedule.test.js

--- App/Test/rotaAddSchedule.test.js
+++ App/Test/rotaAddSchedule.test.js
@@ -9,9 +9,61 @@ describe("POST addschedule ",()=>{
             const response = await request(app)
             .post('/addschedule')
             .set({'Content-Type':'application/json'})
-            .send({date:'21/09/2021',content:'values'})
+            .send({date:'2021/09/21',content:'values'})
 
             expect(response.status).to.equal(200)
+            expect(response.body.msg).to.equal('sucess')
+            expect(response.body.data).to.be.a('array')
+        })
+    })
+
+    describe("addschedule should return a error",()=>{
+        test("should return a invalid  msg and status 400 when send wrong date",async()=>{
+            const response = await request(app)
+            .post('/addschedule')
+            .set({'Content-Type':'application/json'})
+            .send({date:'',content:'values'})
+
+            expect(response.status).to.equal(400)
+            expect(response.body.msg).to.equal('invalid date')
+     
+        })
+        test("should return a error when send invalid day",async()=>{
+            const response = await request(app)
+            .post('/addschedule')
+            .set({'Content-Type':'application/json'})
+            .send({date:'2021/09/2',content:'values'})
+        
+            expect(response.status).to.equal(400)
+            expect(response.body.msg).to.equal('invalid date')
+        })
+        test("should return a error when send invalid month",async()=>{
+            const response = await request(app)
+            .post('/addschedule')
+            .set({'Content-Type':'application/json'})
+            .send({date:'2021/9/2',content:'values'})
+        
+            expect(response.status).to.equal(400)
+            expect(response.body.msg).to.equal('invalid date')
+        })
+        test("should return a error when send invalid year",async()=>{
+            const response = await request(app)
+            .post('/addschedule')
+            .set({'Content-Type':'application/json'})
+            .send({date:'202/09/2',content:'values'})
+        
+            expect(response.status).to.equal(400)
+            expect(response.body.msg).to.equal('invalid date')
+        })
+        test("should return a invalid  msg and status 400 when send wrong content",async()=>{
+            const response = await request(app)
+            .post('/addschedule')
+            .set({'Content-Type':'application/json'})
+            .send({date:'2021/09/21',content:''})
+
+            expect(response.status).to.equal(400)
+            expect(response.body.msg).to.equal('invalid content')
+     
         })
     })
 })
