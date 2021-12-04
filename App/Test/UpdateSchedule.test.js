14fbb2678725839b5228249a77112ba25143fc3f
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:56:56 -0300

    Update UpdateSchedule.test.js

--- App/Test/UpdateSchedule.test.js
+++ App/Test/UpdateSchedule.test.js
@@ -1,23 +1,47 @@
 const {expect} = require('chai')
 const request = require('supertest')
-const app = require('../server')
-
+const server = require('../server')
+const ScheduleDB = require('../model/schedule')
+var app
 
 describe("route /updateschedule",()=>{
-    describe("should return errors tests case",()=>{
-
+    beforeAll(async()=>{
+        app =  server.listen(8084)  
     })
-
+    
+    beforeAll(async()=>{
+        const create = await ScheduleDB.create({
+            id:394,
+            date:'2021-10-23',
+            content:'tam tam'
+        })
+    })
     describe("should return sucessfull tests case",()=>{
-        test("",async()=>{
+        test("should return a error when send wrong values",async()=>{
 
             const response = await request(app)
             .post('/updateschedule')
-            .set({'Content-Type':'application-json'})
-            .send({id:32,content:'bla bla',date:'2021/09/21'})
+            .set({'Content-Type':'application/json'})
+            .send({id:25,content:'bla bla',date:'2021/09/21'})
+
+            expect(response.status).to.equal(402)
+            expect(response.body.msg).to.equal('not found')
+        })
+
+        test("should return a error when send wrong values",async()=>{
+
+            const response = await request(app)
+            .post('/updateschedule')
+            .set({'Content-Type':'application/json'})
+            .send({id:394,content:'bla bla',date:'2021/09/21'})
 
             expect(response.status).to.equal(200)
             expect(response.body.msg).to.equal('sucess')
+        })
+    })
+    afterAll(async()=>{
+        const destroy = await ScheduleDB.destroy({
+            where:{id:200}
         })
     })
 })
