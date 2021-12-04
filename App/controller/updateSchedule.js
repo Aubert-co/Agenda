92bcbb4acd428df3f417fc5cba2c50fb2ffa1206
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:54:57 -0300

    Update updateSchedule.js

--- App/controller/updateSchedule.js
+++ App/controller/updateSchedule.js
@@ -1,8 +1,20 @@
-modu.exports =async function  ApiUpdateSchedule (req,res){
+const Schedule =  require('../model/schedule')
+module.exports =async function  ApiUpdateSchedule (req,res){
     try{
         const {id,content,date} = req.body
 
         if(!id || !content || !date) return res.status(401).send({msg:'wrong values'})
+
+        const dateToDB = date.replace(/[/]/gi,'-')
+        const values = await Schedule.findOne({where:{id}})
+
+        if(values ===null)return res.status(402).send({msg:'not found'})
+
+        const Update = await Schedule.update({
+            dateToDB,
+            content,
+            where:{id}
+        })
 
         res.status(200).send({msg:'sucess'})
     }catch(err){
