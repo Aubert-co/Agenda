e1353a1e860e84f7238a32076bfb75fcee03e151
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:55:28 -0300

    Update SelectAschedule.js

--- App/controller/SelectAschedule.js
+++ App/controller/SelectAschedule.js
@@ -1,6 +1,8 @@
-module.exports = function ApiSelectSchedule(req,res){
+const Schedule = require('../model/schedule')
+module.exports =async function ApiSelectSchedule(req,res){
     try{
-    res.status(200).send({msg:'sucess',data:[]})
+        const data  = await Schedule.findAll()
+        res.status(200).send({msg:'sucess',data})
     }catch(err){
         res.status(401).send({msg:'something went wrong'})
     }
