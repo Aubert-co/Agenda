36f84559bdca301cdee0b8e344d1668e03df1a48
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:55:24 -0300

    Update removeASchedule.js

--- App/controller/removeASchedule.js
+++ App/controller/removeASchedule.js
@@ -1,9 +1,13 @@
+const Schedule = require('../model/schedule')
 module.exports = async function ApiRemoveSchedule(req,res){
     try{
         const {id} = req.body
 
         if(!id || typeof id !=='number')return res.status(401).send({msg:'wrong id'})
 
+        const removeValue = await Schedule.destroy({
+            where:{id}
+        })
         res.status(200).send({msg:'sucess'})
 
     }catch(err){
