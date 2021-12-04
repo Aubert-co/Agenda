c7a2fd84ccef61f37d56a7e8e67e2345327170a3
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:55:41 -0300

    Update index.js

--- App/rotas/index.js
+++ App/rotas/index.js
@@ -2,10 +2,11 @@ const app = require('express').Router()
 const ApiAddSchedule = require('../controller/addASchedule')
 const ApiSelectAschedule = require('../controller/SelectAschedule')
 const ApiRemoveSchedule = require('../controller/removeASchedule')
-
+const ApiUpdateSchedule = require('../controller/updateSchedule')
 app
 .post('/addschedule',ApiAddSchedule)
 .post('/removeschedule',ApiRemoveSchedule)
+.post('/updateschedule',ApiUpdateSchedule)
 .get('/selectschedule',ApiSelectAschedule)
 
 module.exports = app
