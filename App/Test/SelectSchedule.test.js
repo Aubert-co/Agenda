383237244669e1302d1524c4688e0634d7d5b577
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:56:19 -0300

    Update SelectSchedule.test.js

--- App/Test/SelectSchedule.test.js
+++ App/Test/SelectSchedule.test.js
@@ -1,3 +1,22 @@
 const {expect} = require('chai')
 const request = require('supertest')
-const app = require('../server')
+const server = require('../server')
+var app 
+
+
+describe("route /selectschedule",()=>{
+    beforeAll(async()=>{
+        app =  server.listen(8085)  
+    })
+    
+    test("should return a array",async()=>{
+        const response = await request(app)
+        .get('/selectschedule')
+
+        expect(response.body.msg).to.equal('sucess')
+
+        expect(response.status).to.equal(200)
+    })
+
+    
+})
