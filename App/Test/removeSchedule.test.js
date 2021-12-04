6c664d4b9f1a632d6b6d0d70dbcc18636a5d25f9
Black <53127242+Aubert-co@users.noreply.github.com>
2021-12-03 21:55:49 -0300

    Update removeSchedule.test.js

--- App/Test/removeSchedule.test.js
+++ App/Test/removeSchedule.test.js
@@ -1,32 +1,35 @@
 const {expect} = require('chai')
 const request = require('supertest')
-const app = require('../server')
-
+const server = require('../server')
+var app
 describe("route delete schedule",()=>{
+    beforeAll(async()=>{
+        app =  server.listen(8080)  
+    })
     describe("should return errors tests cases",()=>{
-        test("should return a error when send a id undefined",()=>{
+        test("should return a error when send a id undefined",async()=>{
                 const response  =  await request(app)
                 .post('/removeschedule')
                 .set({'Content-Type':'application/json'})
                 .send({id:undefined})
 
                 expect(response.status).to.equal(401)
-                expect(response.body.msg).to.be.equal('something went wrong')
+                expect(response.body.msg).to.be.equal('wrong id')
         })
 
-        test("should return a error when send a id string",()=>{
+        test("should return a error when send a id string",async()=>{
             const response  =  await request(app)
             .post('/removeschedule')
             .set({'Content-Type':'application/json'})
             .send({id:'string'})
 
             expect(response.status).to.equal(401)
-            expect(response.body.msg).to.be.equal('something went wrong')
+            expect(response.body.msg).to.be.equal('wrong id')
         })
     })
 
     describe("should return sucess tests cases",()=>{
-        test("should return a sucessfull msg when send correct values",()=>{
+        test("should return a sucessfull msg when send correct values",async()=>{
             const response = await request(app)
             .post('/removeschedule')
             .set({'Content-Type':'application/json'})
